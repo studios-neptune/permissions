0fb0f1a4125368d8cc022ec851e2b93e7b7a69b1
Babacar Cissé DIA <bcdbuddy@outlook.com>
2022-06-28 13:34:09 +0000

    fix helper

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -6,11 +6,11 @@ if (! function_exists('crud_permissions')) {
     function crud_permissions($prefix): array
     {
         return [
-            "${prefix}_create" => "Can create ${$prefix}",
-            "${prefix}_update" => "Can update ${$prefix}",
-            "${prefix}_delete" => "Can delete ${$prefix}",
-            "${prefix}_view_any" => "Can view any ${$prefix}",
-            "${prefix}_view" => "Can view ${$prefix}",
+            "${prefix}_create" => "Can create ${prefix}",
+            "${prefix}_update" => "Can update ${prefix}",
+            "${prefix}_delete" => "Can delete ${prefix}",
+            "${prefix}_view_any" => "Can view any ${prefix}",
+            "${prefix}_view" => "Can view ${prefix}",
             // TODO: think about adding update_own, update_any, delete_own, delete_any
         ];
     }
